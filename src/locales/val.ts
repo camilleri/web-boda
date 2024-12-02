98b7ec9219f5f6979c6a36e114ae64e38f21f200
Maria Marco Terraes <maria22mt@gmail.com>
2024-12-02 02:01:53 +0100

    Faltava traduccio

--- src/locales/val.ts
+++ src/locales/val.ts
@@ -9,7 +9,7 @@ const val = {
     banner_title: "Maria & David",
     venue_title: "Cerimònia",
     venue_message: "Monestir de Sant Jeroni de Cotalba",
-    gift_title: "Regals",
+    support_title: "Guardiola",
     gift_message: "La vostra presència és el millor regal que ens podeu fer.<br />Tanmateix, si voleu fer ajudar-nos, agraim el vostre suport.",
     transport_title: "Transport",
     transport_message: "Hi haurà transport en autobús des d'Oliva, Gandia i Sueca fins al lloc de l'esdeveniment. Si us plau, fes-nos saber si necessites una plaça al formulari.",
